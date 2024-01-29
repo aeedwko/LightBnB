c55a1a937629c34678f14d8abb366f85b85f773c
Edwin Ko <edwin.mk.ko@gmail.com>
2024-01-29 05:43:38 +0000

    refactor getAllReservations to query lightbnb db

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -73,7 +73,20 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+  .query(`SELECT properties.*
+          FROM reservations
+          JOIN properties ON reservations.property_id = properties.id
+          WHERE reservations.guest_id = $1
+          GROUP BY properties.id, reservations.id
+          ORDER BY reservations.start_date
+          LIMIT $2`, [guest_id, limit])
+  .then((result) => {
+    return result.rows;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 };
 
 /// Properties
