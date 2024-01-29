7bbc6185516d104f5c547e4f7947e24f4d2b7bba
Edwin Ko <edwin.mk.ko@gmail.com>
2024-01-29 01:06:13 +0000

    update users table function to use lightbnb db

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -18,14 +18,14 @@ const users = require("./json/users.json");
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+  return pool
+  .query(`SELECT * FROM users WHERE email = $1`, [email])
+  .then((result) => {
+    return result.rows[0] || null;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 };
 
 /**
@@ -34,7 +34,14 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  return pool
+  .query(`SELECT * FROM users WHERE id = $1`, [id])
+  .then((result) => {
+    return result.rows[0] || null;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 };
 
 /**
@@ -43,10 +50,19 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  console.log(user);
+  const name = user.name;
+  const email = user.email;
+  const password = user.password;
+
+  return pool
+  .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`, [name, email, password])
+  .then((result) => {
+    return result.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 };
 
 /// Reservations
@@ -72,7 +88,6 @@ const getAllProperties = (options, limit = 10) => {
   return pool
     .query(`SELECT * FROM properties LIMIT $1`, [limit])
     .then((result) => {
-      console.log(result.rows);
       return result.rows;
     })
     .catch((err) => {
